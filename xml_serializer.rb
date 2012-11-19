0836f8af554ace97687d7445782bc9b0e83e367e
Sam Serpoosh <ssjesus287@gmail.com>
2012-11-19 02:00:08 -0500

    serialize the collection attributes

--- xml_serializer.rb
+++ xml_serializer.rb
@@ -3,13 +3,27 @@ class XmlSerializer
     @object = object
   end
 
-  def serialize
+  def serialize_class
     class_name = @object.class.name
     "<#{class_name}></#{class_name}>"
   end
 
   def serialize_attribute(attribute_name)
     value = @object.instance_variable_get("@#{attribute_name}")
+    return "" if value.nil?
+
     "<property=\"#{attribute_name}\" value=\"#{value}\" />"
+  end
+
+  def serialize_collection(collection_attribute_name)
+    collection = @object.instance_variable_get("@#{collection_attribute_name}")
+    return "" if collection.empty?
+
+    element = "<collection>"
+    collection.each do |item|
+      element << "<#{item} />"
+    end
+    element << "</collection>"
+    element
   end
 end
--- xml_serializer_spec.rb
+++ xml_serializer_spec.rb
@@ -2,22 +2,42 @@ require_relative "xml_serializer"
 
 describe XmlSerializer do
   it "creates a root xml element with the class name" do
-    serializer = XmlSerializer.new(DummyClass.new(1))
-    serialized = serializer.serialize
+    serializer = XmlSerializer.new(DummyClass.new)
+    serialized = serializer.serialize_class
     serialized.should == "<DummyClass></DummyClass>"
   end
 
   it "creates an xml element for simple property with its value" do
-    object = DummyClass.new(1)
+    object = DummyClass.new
+    object.prop = 1
     serializer = XmlSerializer.new(object)
     serialized = serializer.serialize_attribute("prop")
     serialized.should == "<property=\"prop\" value=\"1\" />"
   end
+
+  it "creates nothing for nil attribute" do
+    object = DummyClass.new
+    object.prop = nil
+    serializer = XmlSerializer.new(object) 
+    serializer.serialize_attribute("prop").should == ""
+  end
+
+  it "creates xml element for collection attribute" do
+    object = DummyClass.new
+    object.collection = [1, 2, "hello"]
+    serializer = XmlSerializer.new(object)
+    serialized = serializer.serialize_collection("collection")
+    serialized.should == "<collection><1 /><2 /><hello /></collection>"
+  end
+
+  it "creates nothing for empty collection" do
+    object = DummyClass.new
+    object.collection = []
+    serializer = XmlSerializer.new(object)
+    serializer.serialize_collection("collection").should == ""
+  end
 end
 
 class DummyClass
-  attr_reader :prop
-  def initialize(prop)
-    @prop = prop
-  end
+  attr_accessor :prop, :collection
 end

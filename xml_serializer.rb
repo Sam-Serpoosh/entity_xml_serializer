df9f8a76e8200ec8dfdb0d06089e24c3a11c6afa
Sam Serpoosh <ssjesus287@gmail.com>
2012-12-04 04:21:40 -0500

    supporting properties which are entity themselves

--- xml_serializer.rb
+++ xml_serializer.rb
@@ -1,32 +1,31 @@
 class XmlSerializer
-  def initialize(entity) 
-    @entity = entity
-  end
+  attr_accessor :entity
 
-  def serialize
-    attributes_names = get_instance_variables
+  def serialize(entity)
+    attributes_names = get_instance_variables(entity)
     serialized_attributes = ""
     attributes_names.each do |attr_name|
-      serialized_attributes += serialize_attribute(attr_name)
+      serialized_attributes += serialize_attribute(entity, attr_name)
     end
-    serialize_entity(serialized_attributes)
+    serialize_entity(entity, serialized_attributes)
   end
 
-  def serialize_attribute(attr_name)
-    attribute = @entity.instance_variable_get("@#{attr_name}")
-    attribute.kind_of?(Array) ? serialize_collection(attr_name)
-                              : serialize_scalar_attribute(attr_name)
+  def serialize_attribute(entity, attr_name)
+    attribute = entity.instance_variable_get("@#{attr_name}")
+    return serialize(attribute) if attribute.kind_of?(Entity)
+    attribute.kind_of?(Array) ? serialize_collection(entity, attr_name)
+                              : serialize_scalar_attribute(entity, attr_name)
   end
 
-  def serialize_scalar_attribute(attribute_name)
-    value = @entity.instance_variable_get("@#{attribute_name}")
+  def serialize_scalar_attribute(entity, attribute_name)
+    value = entity.instance_variable_get("@#{attribute_name}")
     return "" if value.nil?
 
     "<property=\"#{attribute_name}\" value=\"#{value}\" />"
   end
 
-  def serialize_collection(collection_attribute_name)
-    collection = @entity.instance_variable_get("@#{collection_attribute_name}")
+  def serialize_collection(entity, collection_attribute_name)
+    collection = entity.instance_variable_get("@#{collection_attribute_name}")
     return "" if collection.empty?
 
     element = "<collection>"
@@ -37,15 +36,17 @@ class XmlSerializer
     element
   end
 
-  def serialize_entity(serialized_attributes)
-    class_name = @entity.class.name
+  def serialize_entity(entity, serialized_attributes)
+    class_name = entity.class.name
     "<#{class_name}>#{serialized_attributes}</#{class_name}>"
   end
 
   private 
 
-    def get_instance_variables
-      instance_variables = @entity.instance_variables
+    def get_instance_variables(entity)
+      instance_variables = entity.instance_variables
       instance_variables.map { |var| var.to_s.gsub("@", "") }
     end
 end
+
+class Entity; end
--- xml_serializer_spec.rb
+++ xml_serializer_spec.rb
@@ -4,49 +4,73 @@ describe XmlSerializer do
   it "creates an xml element for simple property with its value" do
     entity = DummyEntity.new
     entity.prop = 1
-    serializer = XmlSerializer.new(entity)
-    serialized = serializer.serialize_scalar_attribute("prop")
+    serializer = XmlSerializer.new
+    serialized = serializer.serialize_scalar_attribute(entity, "prop")
     serialized.should == "<property=\"prop\" value=\"1\" />"
   end
 
   it "creates nothing for nil attribute" do
     entity = DummyEntity.new
     entity.prop = nil
-    serializer = XmlSerializer.new(entity) 
-    serializer.serialize_scalar_attribute("prop").should == ""
+    serializer = XmlSerializer.new
+    serializer.serialize_scalar_attribute(entity, "prop").should == ""
   end
 
   it "creates xml element for collection attribute" do
     entity = DummyEntity.new
     entity.collection = [1, 2, "hello"]
-    serializer = XmlSerializer.new(entity)
-    serialized = serializer.serialize_collection("collection")
+    serializer = XmlSerializer.new
+    serialized = serializer.serialize_collection(entity, "collection")
     serialized.should == "<collection><item>1</item><item>2</item><item>hello</item></collection>"
   end
 
   it "creates nothing for empty collection" do
     entity = DummyEntity.new
     entity.collection = []
-    serializer = XmlSerializer.new(entity)
-    serializer.serialize_collection("collection").should == ""
+    serializer = XmlSerializer.new
+    serializer.serialize_collection(entity, "collection").should == ""
   end
 
   it "creates element for entity and embed serialized attributes in it" do
-    serializer = XmlSerializer.new(DummyEntity.new)
-    serialized = serializer.serialize_entity("test")
+    entity = DummyEntity.new
+    serializer = XmlSerializer.new
+    serialized = serializer.serialize_entity(entity, "test")
     serialized.should == "<DummyEntity>test</DummyEntity>"
   end
 
-  it "serialize an entity entity" do
+  it "serialize an entity" do
     entity = DummyEntity.new
     entity.prop = 1
     entity.collection = [1, "hello"]
-    serializer = XmlSerializer.new(entity)
-    serialized = serializer.serialize
+    serializer = XmlSerializer.new
+    serialized = serializer.serialize(entity)
     serialized.should == "<DummyEntity><property=\"prop\" value=\"1\" /><collection><item>1</item><item>hello</item></collection></DummyEntity>"
+  end
+
+  it "serialize the entity property as a child element of main entity" do
+    fake_entity = FakeEntity.new
+    fake_entity.number = 1
+    fake_entity.name = "bob"
+    entity = DummyEntity.new
+    entity.prop = 1
+    entity.collection = [1, "hello"]
+    entity.entity_prop = fake_entity
+    serializer = XmlSerializer.new
+    serialized = serializer.serialize(entity)
+    serialized.should == "<DummyEntity><property=\"prop\" value=\"1\" /><collection><item>1</item><item>hello</item></collection><FakeEntity><property=\"number\" value=\"1\" /><property=\"name\" value=\"bob\" /></FakeEntity></DummyEntity>"
+  end
+
+  def create_serializer(entity)
+    serializer = XmlSerializer.new
+    serializer.entity = entity
+    serializer
   end
 end
 
 class DummyEntity
-  attr_accessor :prop, :collection
+  attr_accessor :prop, :entity_prop, :collection
+end
+
+class FakeEntity < Entity
+  attr_accessor :number, :name
 end
